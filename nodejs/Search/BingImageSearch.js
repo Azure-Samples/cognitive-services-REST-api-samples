9a11352a29aa16b14a3e7a585e51bf7fb84d2b7c
Ronak <rosh@microsoft.com>
2017-12-07 19:18:26 -0800

    Create BingImageSearch.js
    nodejs sample for the Bing Image Search API.

---
+++ nodejs/Search/BingImageSearch.js
@@ -0,0 +1,61 @@
+'use strict';
+
+let https = require('https');
+
+// **********************************************
+// *** Update or verify the following values. ***
+// **********************************************
+
+// Replace the subscriptionKey string value with your valid subscription key.
+let subscriptionKey = 'enter key here';
+
+// Verify the endpoint URI.  At this writing, only one endpoint is used for Bing
+// search APIs.  In the future, regional endpoints may be available.  If you
+// encounter unexpected authorization errors, double-check this host against
+// the endpoint for your Bing Search instance in your Azure dashboard.
+let host = 'api.cognitive.microsoft.com';
+let path = '/bing/v7.0/images/search';
+
+let term = 'puppies';
+
+let response_handler = function (response) {
+    let body = '';
+    response.on('data', function (d) {
+        body += d;
+    });
+    response.on('end', function () {
+        console.log('\nRelevant Headers:\n');
+        for (var header in response.headers)
+            // header keys are lower-cased by Node.js
+            if (header.startsWith("bingapis-") || header.startsWith("x-msedge-"))
+                 console.log(header + ": " + response.headers[header]);
+        body = JSON.stringify(JSON.parse(body), null, '  ');
+        console.log('\nJSON Response:\n');
+        console.log(body);
+    });
+    response.on('error', function (e) {
+        console.log('Error: ' + e.message);
+    });
+};
+
+let bing_image_search = function (search) {
+  console.log('Searching images for: ' + term);
+  let request_params = {
+        method : 'GET',
+        hostname : host,
+        path : path + '?q=' + encodeURIComponent(search),
+        headers : {
+            'Ocp-Apim-Subscription-Key' : subscriptionKey,
+        }
+    };
+
+    let req = https.request(request_params, response_handler);
+    req.end();
+}
+
+if (subscriptionKey.length === 32) {
+    bing_image_search(term);
+} else {
+    console.log('Invalid Bing Search API subscription key!');
+    console.log('Please paste yours into the source code.');
+}
