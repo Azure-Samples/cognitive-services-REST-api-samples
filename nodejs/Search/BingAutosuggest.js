511f7766d41b1fbc6b76861b4feb42c97977a52b
Ronak <rosh@microsoft.com>
2017-12-07 19:24:34 -0800

    Create BingAutosuggest.js
    nodejs sample for the Bing Autosuggest API.

---
+++ nodejs/Search/BingAutosuggest.js
@@ -0,0 +1,49 @@
+'use strict';
+
+let https = require ('https');
+
+// **********************************************
+// *** Update or verify the following values. ***
+// **********************************************
+
+// Replace the subscriptionKey string value with your valid subscription key.
+let subscriptionKey = 'enter key here';
+
+let host = 'api.cognitive.microsoft.com';
+let path = '/bing/v7.0/Suggestions';
+
+let mkt = 'en-US';
+let query = 'sail';
+
+let params = '?mkt=' + mkt + '&q=' + query;
+
+let response_handler = function (response) {
+    let body = '';
+    response.on ('data', function (d) {
+        body += d;
+    });
+    response.on ('end', function () {
+        let body_ = JSON.parse (body);
+        let body__ = JSON.stringify (body_, null, '  ');
+        console.log (body__);
+    });
+    response.on ('error', function (e) {
+        console.log ('Error: ' + e.message);
+    });
+};
+
+let get_suggestions = function () {
+    let request_params = {
+        method : 'GET',
+        hostname : host,
+        path : path + params,
+        headers : {
+            'Ocp-Apim-Subscription-Key' : subscriptionKey,
+        }
+    };
+
+    let req = https.request (request_params, response_handler);
+    req.end ();
+}
+
+get_suggestions ();
