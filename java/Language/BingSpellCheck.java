926e94fc5c32eec43a3c7f4739cdf143508883a0
Ronak <rosh@microsoft.com>
2017-12-07 20:30:38 -0800

    Create BingSpellCheck.java
    java sample for the Bing Spell Check API.

---
+++ java/Language/BingSpellCheck.java
@@ -0,0 +1,52 @@
+/*Copyright (c) Microsoft Corporation. All rights reserved.
+Licensed under the MIT License.*/
+
+import java.io.*;
+import java.net.*;
+import javax.net.ssl.HttpsURLConnection;
+
+public class HelloWorld {
+
+    static String host = "https://api.cognitive.microsoft.com";
+    static String path = "/bing/v7.0/spellcheck";
+
+    // NOTE: Replace this example key with a valid subscription key.
+    static String key = "ENTER KEY HERE";
+
+    static String mkt = "en-US";
+    static String mode = "proof";
+    static String text = "Hollo, wrld!";
+
+    public static void check () throws Exception {
+        String params = "?mkt=" + mkt + "&mode=" + mode;
+        URL url = new URL(host + path + params);
+        HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
+        connection.setRequestMethod("POST");
+        connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+        connection.setRequestProperty("Content-Length", "" + text.length() + 5);
+        connection.setRequestProperty("Ocp-Apim-Subscription-Key", key);
+        connection.setDoOutput(true);
+
+        DataOutputStream wr = new DataOutputStream(connection.getOutputStream());
+        wr.writeBytes("text=" + text);
+        wr.flush();
+        wr.close();
+
+        BufferedReader in = new BufferedReader(
+        new InputStreamReader(connection.getInputStream()));
+        String line;
+        while ((line = in.readLine()) != null) {
+            System.out.println(line);
+        }
+        in.close();
+    }
+
+    public static void main(String[] args) {
+        try {
+            check ();
+        }
+        catch (Exception e) {
+            System.out.println (e);
+        }
+    }
+}
