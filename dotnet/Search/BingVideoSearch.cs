da164ddd3b7564c3857fcf476f91044036b72e8c
Ronak <rosh@microsoft.com>
2017-12-07 18:58:02 -0800

    Create BingVideoSearch.cs
    dotnet sample for the Bing Video Search API.

---
+++ dotnet/Search/BingVideoSearch.cs
@@ -0,0 +1,148 @@
+using System;
+using System.Text;
+using System.Net;
+using System.IO;
+using System.Collections.Generic;
+
+namespace BingVideoSearchCSharpCore
+{
+
+    class Program
+    {
+        // **********************************************
+        // *** Update or verify the following values. ***
+        // **********************************************
+
+        // Replace the accessKey string value with your valid access key.
+        const string accessKey = "enter key here";
+
+        // Verify the endpoint URI.  At this writing, only one endpoint is used for Bing
+        // search APIs.  In the future, regional endpoints may be available.  If you
+        // encounter unexpected authorization errors, double-check this value against
+        // the endpoint for your Bing search instance in your Azure dashboard.
+        const string uriBase = "https://api.cognitive.microsoft.com/bing/v7.0/videos/search";
+
+        const string searchTerm = "kittens";
+
+        // Used to return video search results including relevant headers
+        struct SearchResult
+        {
+            public String jsonResult;
+            public Dictionary<String, String> relevantHeaders;
+        }
+
+        static void Main()
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Searching videos for: " + searchTerm);
+
+            SearchResult result = BingVideoSearch(searchTerm);
+
+            Console.WriteLine("\nRelevant HTTP Headers:\n");
+            foreach (var header in result.relevantHeaders)
+                Console.WriteLine(header.Key + ": " + header.Value);
+
+            Console.WriteLine("\nJSON Response:\n");
+            Console.WriteLine(JsonPrettyPrint(result.jsonResult));
+
+            Console.Write("\nPress Enter to exit ");
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Performs a Bing video search and return the results as a SearchResult.
+        /// </summary>
+        static SearchResult BingVideoSearch(string searchQuery)
+        {
+            // Construct the URI of the search request
+            var uriQuery = uriBase + "?q=" + Uri.EscapeDataString(searchQuery);
+
+            // Perform the Web request and get the response
+            WebRequest request = HttpWebRequest.Create(uriQuery);
+            request.Headers["Ocp-Apim-Subscription-Key"] = accessKey;
+            HttpWebResponse response = (HttpWebResponse)request.GetResponseAsync().Result;
+            string json = new StreamReader(response.GetResponseStream()).ReadToEnd();
+
+            // Create result object for return
+            var searchResult = new SearchResult();
+            searchResult.jsonResult = json;
+            searchResult.relevantHeaders = new Dictionary<String, String>();
+
+            // Extract Bing HTTP headers
+            foreach (String header in response.Headers)
+            {
+                if (header.StartsWith("BingAPIs-") || header.StartsWith("X-MSEdge-"))
+                    searchResult.relevantHeaders[header] = response.Headers[header];
+            }
+
+            return searchResult;
+        }
+
+        /// <summary>
+        /// Formats the given JSON string by adding line breaks and indents.
+        /// </summary>
+        /// <param name="json">The raw JSON string to format.</param>
+        /// <returns>The formatted JSON string.</returns>
+        static string JsonPrettyPrint(string json)
+        {
+            if (string.IsNullOrEmpty(json))
+                return string.Empty;
+
+            json = json.Replace(Environment.NewLine, "").Replace("\t", "");
+
+            StringBuilder sb = new StringBuilder();
+            bool quote = false;
+            bool ignore = false;
+            int offset = 0;
+            int indentLength = 3;
+
+            foreach (char ch in json)
+            {
+                switch (ch)
+                {
+                    case '"':
+                        if (!ignore) quote = !quote;
+                        break;
+                    case '\'':
+                        if (quote) ignore = !ignore;
+                        break;
+                }
+
+                if (quote)
+                    sb.Append(ch);
+                else
+                {
+                    switch (ch)
+                    {
+                        case '{':
+                        case '[':
+                            sb.Append(ch);
+                            sb.Append(Environment.NewLine);
+                            sb.Append(new string(' ', ++offset * indentLength));
+                            break;
+                        case '}':
+                        case ']':
+                            sb.Append(Environment.NewLine);
+                            sb.Append(new string(' ', --offset * indentLength));
+                            sb.Append(ch);
+                            break;
+                        case ',':
+                            sb.Append(ch);
+                            sb.Append(Environment.NewLine);
+                            sb.Append(new string(' ', offset * indentLength));
+                            break;
+                        case ':':
+                            sb.Append(ch);
+                            sb.Append(' ');
+                            break;
+                        default:
+                            if (ch != ' ') sb.Append(ch);
+                            break;
+                    }
+                }
+            }
+
+            return sb.ToString().Trim();
+        }
+    }
+}
