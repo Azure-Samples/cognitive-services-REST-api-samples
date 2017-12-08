4b5389a6d56368156fc84bf14e6d6dc6300be232
Ronak <rosh@microsoft.com>
2017-12-07 21:23:58 -0800

    Create BingVideoSearch.py
    python sample for the Bing Video Search API.

---
+++ python/Search/BingVideoSearch.py
@@ -0,0 +1,42 @@
+#Copyright (c) Microsoft Corporation. All rights reserved.
+#Licensed under the MIT License.
+
+# -*- coding: utf-8 -*-
+
+import http.client, urllib.parse, json
+
+# **********************************************
+# *** Update or verify the following values. ***
+# **********************************************
+
+# Replace the subscriptionKey string value with your valid subscription key.
+subscriptionKey = "enter key here"
+
+# Verify the endpoint URI.  At this writing, only one endpoint is used for Bing
+# search APIs.  In the future, regional endpoints may be available.  If you
+# encounter unexpected authorization errors, double-check this value against
+# the endpoint for your Bing search instance in your Azure dashboard.
+host = "api.cognitive.microsoft.com"
+path = "/bing/v7.0/videos/search"
+
+term = "kittens"
+
+def BingVideoSearch(search):
+    "Performs a Bing video search and returns the results."
+
+    headers = {'Ocp-Apim-Subscription-Key': subscriptionKey}
+    conn = http.client.HTTPSConnection(host)
+    query = urllib.parse.quote(search)
+    conn.request("GET", path + "?q=" + query, headers=headers)
+    response = conn.getresponse()
+    headers = [k + ": " + v for (k, v) in response.getheaders()
+                   if k.startswith("BingAPIs-") or k.startswith("X-MSEdge-")]
+    return headers, response.read().decode("utf8")
+
+print('Searching videos for: ', term)
+
+headers, result = BingVideoSearch(term)
+print("\nRelevant HTTP Headers:\n")
+print("\n".join(headers))
+print("\nJSON Response:\n")
+print(json.dumps(json.loads(result), indent=4))
