99fbfa605b40bab9a19edb782b14a9768f1ff8a9
Ronak <rosh@microsoft.com>
2017-12-07 21:29:17 -0800

    Create BingAutosuggest.py
    python sample for the Bing Autosuggest API.

---
+++ python/Search/BingAutosuggest.py
@@ -0,0 +1,33 @@
+#Copyright (c) Microsoft Corporation. All rights reserved.
+#Licensed under the MIT License.
+
+# -*- coding: utf-8 -*-
+
+import http.client, urllib.parse, json
+
+# **********************************************
+# *** Update or verify the following values. ***
+# **********************************************
+
+# Replace the subscriptionKey string value with your valid subscription key.
+subscriptionKey = 'enter key here'
+
+host = 'api.cognitive.microsoft.com'
+path = '/bing/v7.0/Suggestions'
+
+mkt = 'en-US'
+query = 'sail'
+
+params = '?mkt=' + mkt + '&q=' + query
+
+def get_suggestions ():
+    "Gets Autosuggest results for a query and returns the information."
+
+    headers = {'Ocp-Apim-Subscription-Key': subscriptionKey}
+    conn = http.client.HTTPSConnection(host)
+    conn.request ("GET", path + params, None, headers)
+    response = conn.getresponse ()
+    return response.read ()
+
+result = get_suggestions ()
+print (json.dumps(json.loads(result), indent=4))
