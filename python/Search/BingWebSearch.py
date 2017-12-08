18cdd7abf8ba71e3ed93a1dcbb29b4f70035344c
Ronak <rosh@microsoft.com>
2017-12-07 21:21:22 -0800

    Create BingWebSearch.py
    Python sample for the Bing Web Search API.

---
+++ python/Search/BingWebSearch.py
@@ -0,0 +1,49 @@
+#Copyright (c) Microsoft Corporation. All rights reserved.
+#Licensed under the MIT License.
+
+# -*- coding: utf-8 -*-
+
+import http.client, urllib.parse, json
+
+# **********************************************
+# *** Update or verify the following values. ***
+# **********************************************
+
+# Replace the subscriptionKey string value with your valid subscription key.
+subscriptionKey = "enter key here"
+
+# Verify the endpoint URI.  At this writing, only one endpoint is used for Bing
+# search APIs.  In the future, regional endpoints may be available.  If you
+# encounter unexpected authorization errors, double-check this value against
+# the endpoint for your Bing Web search instance in your Azure dashboard.
+host = "api.cognitive.microsoft.com"
+path = "/bing/v7.0/search"
+
+term = "Microsoft Cognitive Services"
+
+def BingWebSearch(search):
+    "Performs a Bing Web search and returns the results."
+
+    headers = {'Ocp-Apim-Subscription-Key': subscriptionKey}
+    conn = http.client.HTTPSConnection(host)
+    query = urllib.parse.quote(search)
+    conn.request("GET", path + "?q=" + query, headers=headers)
+    response = conn.getresponse()
+    headers = [k + ": " + v for (k, v) in response.getheaders()
+                   if k.startswith("BingAPIs-") or k.startswith("X-MSEdge-")]
+    return headers, response.read().decode("utf8")
+
+if len(subscriptionKey) == 32:
+
+    print('Searching the Web for: ', term)
+
+    headers, result = BingWebSearch(term)
+    print("\nRelevant HTTP Headers:\n")
+    print("\n".join(headers))
+    print("\nJSON Response:\n")
+    print(json.dumps(json.loads(result), indent=4))
+
+else:
+
+    print("Invalid Bing Search API subscription key!")
+    print("Please paste yours into the source code.")
