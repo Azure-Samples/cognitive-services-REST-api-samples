93e0c1a565a6f565418b933fd0b35ede8e423a33
Ronak <rosh@microsoft.com>
2017-12-07 19:03:17 -0800

    Create BingAutosuggest.cs
    dotnet sample for the Bing Autosuggest API.

---
+++ dotnet/Search/BingAutosuggest.cs
@@ -0,0 +1,121 @@
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace AutosuggestSample1
+{
+    class Program
+    {
+        static string host = "https://api.cognitive.microsoft.com";
+        static string path = "/bing/v7.0/Suggestions";
+
+        // For a list of available markets, go to:
+        // https://docs.microsoft.com/rest/api/cognitiveservices/bing-autosuggest-api-v7-reference#market-codes
+        static string market = "en-US";
+
+        // NOTE: Replace this example key with a valid subscription key.
+                static string key = "INSERT API KEY";
+
+        static string query = "sail";
+
+        // These properties are used for optional headers (see below).
+        // static string ClientId = "<Client ID from Previous Response Goes Here>";
+        // static string ClientIp = "999.999.999.999";
+        // static string ClientLocation = "+90.0000000000000;long: 00.0000000000000;re:100.000000000000";
+
+        async static void Autosuggest()
+        {
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", key);
+
+            // The following headers are optional, but it is recommended they be treated as required.
+            // These headers help the service return more accurate results.
+            //client.DefaultRequestHeaders.Add("X-Search-Location", ClientLocation);
+            //client.DefaultRequestHeaders.Add("X-MSEdge-ClientID", ClientId);
+            //client.DefaultRequestHeaders.Add("X-MSEdge-ClientIP", ClientIp);
+
+            string uri = host + path + "?mkt=" + market + "&query=" + System.Net.WebUtility.UrlEncode (query);
+
+            HttpResponseMessage response = await client.GetAsync(uri);
+
+            string contentString = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(JsonPrettyPrint(contentString));
+        }
+
+        static void Main(string[] args)
+        {
+            Autosuggest();
+            Console.ReadLine();
+        }
+
+        static string JsonPrettyPrint(string json)
+        {
+            if (string.IsNullOrEmpty(json)) {
+                return string.Empty;
+            }
+
+            json = json.Replace(Environment.NewLine, "").Replace("\t", "");
+
+            StringBuilder sb = new StringBuilder();
+            bool quote = false;
+            bool ignore = false;
+            char last = ' ';
+            int offset = 0;
+            int indentLength = 3;
+
+            foreach (char ch in json)
+            {
+                switch (ch)
+                {
+                    case '"':
+                        if (!ignore) quote = !quote;
+                        break;
+                    case '\\':
+                        if (quote && last != '\\') ignore = true;
+                        break;
+                }
+
+                if (quote)
+                {
+                    sb.Append(ch);
+                    if (last == '\\' && ignore) ignore = false;
+                }
+                else
+                {
+                    switch (ch)
+                    {
+                        case '{':
+                        case '[':
+                            sb.Append(ch);
+                            sb.Append(Environment.NewLine);
+                            sb.Append(new string(' ', ++offset * indentLength));
+                            break;
+                        case '}':
+                        case ']':
+                            sb.Append(Environment.NewLine);
+                            sb.Append(new string(' ', --offset * indentLength));
+                            sb.Append(ch);
+                            break;
+                        case ',':
+                            sb.Append(ch);
+                            sb.Append(Environment.NewLine);
+                            sb.Append(new string(' ', offset * indentLength));
+                            break;
+                        case ':':
+                            sb.Append(ch);
+                            sb.Append(' ');
+                            break;
+                        default:
+                            if (quote || ch != ' ') sb.Append(ch);
+                            break;
+                    }
+                }
+                last = ch;
+            }
+
+            return sb.ToString().Trim();
+        }
+    }
+}
