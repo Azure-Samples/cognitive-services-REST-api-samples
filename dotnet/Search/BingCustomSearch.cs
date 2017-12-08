5a921886fe3da9b01233cb6e2daa174a6445aa4a
Ronak <rosh@microsoft.com>
2017-12-07 19:01:42 -0800

    Create BingCustomSearch.cs
    dotnet sample for the Bing Custom Search API.

---
+++ dotnet/Search/BingCustomSearch.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Net.Http;
+using System.Web;
+using Newtonsoft.Json;
+
+namespace bing_custom_search_example_dotnet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var subscriptionKey = "YOUR-SUBSCRIPTION-KEY";
+            var customConfigId = "YOUR-CUSTOM-CONFIG-ID";
+            var searchTerm = args.Length > 0 ? args[0]: "microsoft";            
+
+            var url = "https://api.cognitive.microsoft.com/bingcustomsearch/v7.0/search?" +
+                "q=" + searchTerm +
+                "&customconfig=" + customConfigId;
+
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", subscriptionKey);
+            var httpResponseMessage = client.GetAsync(url).Result;
+            var responseContent = httpResponseMessage.Content.ReadAsStringAsync().Result;
+            BingCustomSearchResponse response = JsonConvert.DeserializeObject<BingCustomSearchResponse>(responseContent);
+
+            for(int i = 0; i < response.webPages.value.Length; i++)
+            {                
+                var webPage = response.webPages.value[i];
+
+                Console.WriteLine("name: " + webPage.name);
+                Console.WriteLine("url: " + webPage.url);                
+                Console.WriteLine("displayUrl: " + webPage.displayUrl);
+                Console.WriteLine("snippet: " + webPage.snippet);
+                Console.WriteLine("dateLastCrawled: " + webPage.dateLastCrawled);
+                Console.WriteLine();
+            }            
+        }
+    }
+
+    public class BingCustomSearchResponse
+    {        
+        public string _type{ get; set; }            
+        public WebPages webPages { get; set; }
+    }
+
+    public class WebPages
+    {
+        public string webSearchUrl { get; set; }
+        public int totalEstimatedMatches { get; set; }
+        public WebPage[] value { get; set; }        
+    }
+
+    public class WebPage
+    {
+        public string name { get; set; }
+        public string url { get; set; }
+        public string displayUrl { get; set; }
+        public string snippet { get; set; }
+        public DateTime dateLastCrawled { get; set; }
+        public string cachedPageUrl { get; set; }
+        public OpenGraphImage openGraphImage { get; set; }        
+    }
+
+    public class OpenGraphImage
+    {
+        public string contentUrl { get; set; }
+        public int width { get; set; }
+        public int height { get; set; }
+    }
+}
