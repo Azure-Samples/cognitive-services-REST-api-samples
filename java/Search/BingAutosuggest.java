6c6ee9c173909bb8788b86f8b60a49d8622b2238
Ronak <rosh@microsoft.com>
2017-12-07 20:28:29 -0800

    Create BingAutosuggest.java
    java sample for the Bing Autosuggest API.

---
+++ java/Search/BingAutosuggest.java
@@ -0,0 +1,81 @@
+/*Copyright (c) Microsoft Corporation. All rights reserved.
+Licensed under the MIT License.*/
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import javax.net.ssl.HttpsURLConnection;
+
+/*
+ * Gson: https://github.com/google/gson
+ * Maven info:
+ *     groupId: com.google.code.gson
+ *     artifactId: gson
+ *     version: 2.8.1
+ *
+ * Once you have compiled or downloaded gson-2.8.1.jar, assuming you have placed it in the
+ * same folder as this file (Autosuggest.java), you can compile and run this program at
+ * the command line as follows.
+ *
+ * javac Autosuggest.java -classpath .;gson-2.8.1.jar -encoding UTF-8
+ * java -cp .;gson-2.8.1.jar Autosuggest
+ */
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+public class Autosuggest {
+
+// **********************************************
+// *** Update or verify the following values. ***
+// **********************************************
+
+// Replace the subscriptionKey string value with your valid subscription key.
+    static String subscriptionKey = "enter key here";
+
+    static String host = "https://api.cognitive.microsoft.com";
+    static String path = "/bing/v7.0/Suggestions";
+
+    static String mkt = "en-US";
+    static String query = "sail";
+
+    public static String get_suggestions () throws Exception {
+        String encoded_query = URLEncoder.encode (query, "UTF-8");
+        String params = "?mkt=" + mkt + "&q=" + encoded_query;
+        URL url = new URL (host + path + params);
+
+        HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+        connection.setRequestProperty("Ocp-Apim-Subscription-Key", subscriptionKey);
+        connection.setDoOutput(true);
+
+        StringBuilder response = new StringBuilder ();
+        BufferedReader in = new BufferedReader(
+        new InputStreamReader(connection.getInputStream()));
+        String line;
+        while ((line = in.readLine()) != null) {
+            response.append(line);
+        }
+        in.close();
+
+        return response.toString();
+    }
+
+    public static String prettify (String json_text) {
+        JsonParser parser = new JsonParser();
+        JsonObject json = parser.parse(json_text).getAsJsonObject();
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        return gson.toJson(json);
+    }
+
+    public static void main(String[] args) {
+        try {
+            String response = get_suggestions ();
+            System.out.println (prettify (response));
+        }
+        catch (Exception e) {
+            System.out.println (e);
+        }
+    }
+}
