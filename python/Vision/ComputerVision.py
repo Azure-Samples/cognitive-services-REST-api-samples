8553f7e25baa4b7dcd23cfaaec90ad6616e5444a
Ronak <rosh@microsoft.com>
2017-12-14 13:59:37 -0800

    Create ComputerVision.py
    Added ComputerVision example for py.

---
+++ python/Vision/ComputerVision.py
@@ -0,0 +1,54 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+########### Python 3.6 #############
+import http.client, urllib.request, urllib.parse, urllib.error, base64, json
+
+###############################################
+#### Update or verify the following values. ###
+###############################################
+
+# Replace the subscription_key string value with your valid subscription key.
+subscription_key = 'Enter key here'
+
+# Replace or verify the region.
+#
+# You must use the same region in your REST API call as you used to obtain your subscription keys.
+# For example, if you obtained your subscription keys from the westus region, replace 
+# "westcentralus" in the URI below with "westus".
+#
+# NOTE: Free trial subscription keys are generated in the westcentralus region, so if you are using
+# a free trial subscription key, you should not need to change this region.
+uri_base = 'westcentralus.api.cognitive.microsoft.com'
+
+headers = {
+    # Request headers.
+    'Content-Type': 'application/json',
+    'Ocp-Apim-Subscription-Key': subscription_key,
+}
+
+params = urllib.parse.urlencode({
+    # Request parameters. All of them are optional.
+    'visualFeatures': 'Categories,Description,Color',
+    'language': 'en',
+})
+
+# Replace the three dots below with the URL of a JPEG image of a celebrity.
+body = "{'url':'https://upload.wikimedia.org/wikipedia/commons/1/12/Broadway_and_Times_Square_by_night.jpg'}"
+
+try:
+    # Execute the REST API call and get the response.
+    conn = http.client.HTTPSConnection('westcentralus.api.cognitive.microsoft.com')
+    conn.request("POST", "/vision/v1.0/analyze?%s" % params, body, headers)
+    response = conn.getresponse()
+    data = response.read()
+
+    # 'data' contains the JSON data. The following formats the JSON data for display.
+    parsed = json.loads(data)
+    print ("Response:")
+    print (json.dumps(parsed, sort_keys=True, indent=2))
+    conn.close()
+
+except Exception as e:
+    print('Error:')
+    print(e)
