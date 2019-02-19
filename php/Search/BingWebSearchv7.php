1e62a41725c6e9c26dc01350f73366a43f9c0d06
Aaron Hill <aahi@microsoft.com>
2019-02-19 09:41:15 -0800

    editing

--- php/Search/BingWebSearchv7.php
+++ php/Search/BingWebSearchv7.php
@@ -1,6 +1,6 @@
 <?php
 // Replace with a valid subscription key from your Azure account.
-$accessKey = '6911fc5837bf4c7d83b18233ea8c25eb';
+$accessKey = 'enter key here';
 $endpoint = 'https://api.cognitive.microsoft.com/bing/v7.0/search';
 $term = 'Microsoft Cognitive Services';
 
