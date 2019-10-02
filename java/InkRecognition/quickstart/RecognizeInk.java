a51633766f8de5a2f260273aaf2e6687e5c02558
Aaron Hill <aahi@microsoft.com>
2019-10-02 14:41:46 -0700

    adding code tags

--- java/InkRecognition/quickstart/RecognizeInk.java
+++ java/InkRecognition/quickstart/RecognizeInk.java
@@ -1,6 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-
+// <imports>
 import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
@@ -11,29 +11,33 @@ import org.apache.http.util.EntityUtils;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+// </imports>
 
 public class RecognizeInk {
-
+    // <vars>
     // Replace the subscriptionKey string value with your valid subscription key.
     private static final String subscriptionKey = "YOUR_SUBSCRIPTION_KEY";
     // Replace the dataPath string with a path to the JSON formatted ink stroke data file.
     public static final String rootUrl = "https://api.cognitive.microsoft.com";
     public static final String inkRecognitionUrl = "/inkrecognizer/v1.0-preview/recognize";
-    private static final String dataPath = "PATH_TO_INK_STROKE_DATA";
-
+    private static final String dataPath = "C:\\Users\\aahil\\Code\\work\\cognitive-services-REST-api-samples\\java\\InkRecognition\\quickstart\\example-response.json";
+    // </vars>
+    // <main>
     public static void main(String[] args) throws Exception {
 
         String requestData = new String(Files.readAllBytes(Paths.get(dataPath)), "utf-8");
         recognizeInk(requestData);
     }
-
+    // </main>
+    // <recognizeInk>
     static void recognizeInk(String requestData) {
         System.out.println("Sending an Ink recognition request.");
 
         String result = sendRequest(rootUrl, inkRecognitionUrl, subscriptionKey, requestData);
         System.out.println(result);
     }
-
+    // </recognizeInk>
+    // <sendRequest>
     static String sendRequest(String endpoint, String apiAddress, String subscriptionKey, String requestData) {
         try (CloseableHttpClient client = HttpClients.createDefault()) {
             HttpPost request = new HttpPost(endpoint + apiAddress);
@@ -55,4 +59,5 @@ public class RecognizeInk {
         }
         return null;
     }
+    // </sendRequest>
 }
