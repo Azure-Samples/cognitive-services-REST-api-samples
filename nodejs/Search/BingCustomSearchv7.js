168ed62611d5db803b79897809fc35f27bcdde6d
Aaron Hill <aahi@microsoft.com>
2019-10-29 13:59:58 -0700

    updating node js quickstart

--- nodejs/Search/BingCustomSearchv7.js
+++ nodejs/Search/BingCustomSearchv7.js
@@ -1,21 +1,26 @@
 //Copyright (c) Microsoft Corporation. All rights reserved.
 //Licensed under the MIT License.
-
+// <vars>
 var request = require("request");
-
-var subscriptionKey = process.env['BING_CUSTOM_SEARCH_SUBSCRIPTION_KEY']
-var customConfigId = 'YOUR-CUSTOM-CONFIG-ID';
+// Add your Bing Custom Search subscription key to your environment variables.
+// Your endpoint will have the form: 
+//   https://<your-custom-subdomain>.cognitiveservices.azure.com/bingcustomsearch/v7.0
+var subscriptionKey = process.env['BING_CUSTOM_SEARCH_SUBSCRIPTION_KEY'];
+var endpoint = process.env['BING_CUSTOM_SEARCH_ENDPOINT'];
+var customConfigId = 'YOUR-CUSTOM-CONFIG-ID'; //you can also use "1"
 var searchTerm = 'microsoft';
-
+// </vars>
+// <requestOptions>
 var options = {
-    url: process.env['BING_CUSTOM_SEARCH_ENDPOINT'] + "/bingcustomsearch/v7.0/search?' + 
+    url: endpoint + "/search?" + 
       'q=' + searchTerm + 
       '&customconfig=' + customConfigId,
     headers: {
         'Ocp-Apim-Subscription-Key' : subscriptionKey
     }
 }
-
+// </requestOptions>
+// <requestMethod>
 request(options, function(error, response, body){
     var searchResponse = JSON.parse(body);
     for(var i = 0; i < searchResponse.webPages.value.length; ++i){
@@ -28,3 +33,4 @@ request(options, function(error, response, body){
         console.log();
     }
 })
+// </requestMethod>
