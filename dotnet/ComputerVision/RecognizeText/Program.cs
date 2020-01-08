08b83d78d7f9e960bd826c43f3e965f1eb6f0533
Patrick Farley <pafarley@microsoft.com>
2020-01-08 10:01:06 -0800

    fix env vars; use await-async fully

--- dotnet/ComputerVision/RecognizeText/Program.cs
+++ dotnet/ComputerVision/RecognizeText/Program.cs
@@ -11,12 +11,12 @@ namespace Microsoft.Azure.CognitiveServices.Samples.ComputerVision.RecognizeText
 
     public class Program
     {
-        public const string subscriptionKey = Environment.GetEnvironmentVariable("COMPUTER_VISION_SUBSCRIPTION_KEY");
-        public const string endpoint = Environment.GetEnvironmentVariable("COMPUTER_VISION_ENDPOINT");
+        public static string subscriptionKey = Environment.GetEnvironmentVariable("COMPUTER_VISION_SUBSCRIPTION_KEY");
+        public static string endpoint = Environment.GetEnvironmentVariable("COMPUTER_VISION_ENDPOINT");
         
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            RecognizeTextSample.RunAsync(endpoint, subscriptionKey).Wait(5000);
+            await RecognizeTextSample.RunAsync(endpoint, subscriptionKey);
 
             Console.WriteLine("\nPress ENTER to exit.");
             Console.ReadLine();
--- dotnet/ComputerVision/TagImage/Program.cs
+++ dotnet/ComputerVision/TagImage/Program.cs
@@ -11,12 +11,12 @@ namespace Microsoft.Azure.CognitiveServices.Samples.ComputerVision.TagImage
     class Program
     {
         // Add your Azure Computer Vision subscription key and endpoint to your environment variables
-        public const string subscriptionKey = Environment.GetEnvironmentVariable("COMPUTER_VISION_SUBSCRIPTION_KEY");
-        public const string endpoint = Environment.GetEnvironmentVariable("COMPUTER_VISION_ENDPOINT");
+        public static string subscriptionKey = Environment.GetEnvironmentVariable("COMPUTER_VISION_SUBSCRIPTION_KEY");
+        public static string endpoint = Environment.GetEnvironmentVariable("COMPUTER_VISION_ENDPOINT");
         
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            TagImageSample.RunAsync(endpoint, subscriptionKey).Wait(5000);
+            await TagImageSample.RunAsync(endpoint, subscriptionKey);
 
             Console.WriteLine("\nPress ENTER to exit.");
             Console.ReadLine();
