b32b1550fc46d2e9b781713333c8334b4c82a5d4
Winona Azure <38537084+wiazur@users.noreply.github.com>
2020-01-22 18:33:29 -0800

    Updated structure

--- dotnet/Search/BingVisualSearchv7.cs
+++ dotnet/Search/BingVisualSearchv7.cs
@@ -1,25 +1,24 @@
+using Newtonsoft.Json;
 using System;
 using System.Text;
 using System.Net;
 using System.IO;
-using System.Collections.Generic;
 
-namespace VisualSearchUpload
+/* This sample makes a call to the Bing Visual Search API with a query image and returns similar images with details.
+ * Bing Visual Search API: 
+ * https://docs.microsoft.com/en-us/rest/api/cognitiveservices/bingvisualsearch/images/visualsearch
+ */
+
+namespace BingVisualSearch
 {
-
     class Program
     {
-        // **********************************************
-        // *** Update and verify the following values. ***
-        // **********************************************
-
-        // Add your Azure Bing Search V7 subscription key to your environment variables.
-        const string accessKey = Environment.GetEnvironmentVariable("BING_SEARCH_V7_SUBSCRIPTION_KEY");
-        // Add your Azure Bing Search V7 endpoint to your environment variables.
-        const string uriBase = Environment.GetEnvironmentVariable("BING_SEARCH_V7_ENDPOINT") + "/bing/v7.0/images/visualsearch";
+        // Add your Azure Bing Search V7 subscription key and endpoint to your environment variables.
+        static string subscriptionKey = Environment.GetEnvironmentVariable("BING_SEARCH_V7_SUBSCRIPTION_KEY");
+        static string endpoint = Environment.GetEnvironmentVariable("BING_SEARCH_V7_ENDPOINT") + "/bing/v7.0/images/visualsearch";
 
         // Set the path to the image that you want to get insights of. 
-        static string imagePath = @"<pathtoimagegoeshere>";
+        static string imagePath = @"objects.jpg";
 
         // Boundary strings for form data in body of POST.
         const string CRLF = "\r\n";
@@ -28,216 +27,63 @@ namespace VisualSearchUpload
         static string EndBoundaryTemplate = "--{0}--";
 
         const string CONTENT_TYPE_HEADER_PARAMS = "multipart/form-data; boundary={0}";
-        const string POST_BODY_DISPOSITION_HEADER = "Content-Disposition: form-data; name=\"image\"; filename=\"{0}\"" + CRLF +CRLF;
+        const string POST_BODY_DISPOSITION_HEADER = "Content-Disposition: form-data; name=\"image\"; filename=\"{0}\"" + CRLF + CRLF;
 
 
         static void Main()
         {
             try
             {
-                Console.OutputEncoding = System.Text.Encoding.UTF8;
+                Console.OutputEncoding = Encoding.UTF8;
 
-                if (accessKey.Length == 32)
+                // Gets image.
+                var filename = new FileInfo(imagePath).FullName;
+                Console.WriteLine("Getting image insights for image: " + Path.GetFileName(filename));
+                var imageBinary = File.ReadAllBytes(imagePath);
+
+                // Sets up POST body.
+                var boundary = string.Format(BoundaryTemplate, Guid.NewGuid());
+
+                // Builds form start data.
+                var startBoundary = string.Format(StartBoundaryTemplate, boundary);
+                var startFormData = startBoundary + CRLF;
+                startFormData += string.Format(POST_BODY_DISPOSITION_HEADER, filename);
+
+                // Builds form end data.
+                var endFormData = CRLF + CRLF + string.Format(EndBoundaryTemplate, boundary) + CRLF;
+                var contentTypeHeaderValue = string.Format(CONTENT_TYPE_HEADER_PARAMS, boundary);
+
+                // Sets up the request for a visual search.
+                WebRequest request = HttpWebRequest.Create(endpoint);
+                request.ContentType = contentTypeHeaderValue;
+                request.Headers["Ocp-Apim-Subscription-Key"] = subscriptionKey;
+                request.Method = "POST";
+
+                // Writes the boundary and Content-Disposition header, then writes
+                // the image binary, and finishes by writing the closing boundary.
+                using (Stream requestStream = request.GetRequestStream())
                 {
-                    if (IsImagePathSet(imagePath))
-                    {
-                        var filename = GetImageFileName(imagePath);
-                        Console.WriteLine("Getting image insights for image: " + filename);
-                        var imageBinary = GetImageBinary(imagePath);
-
-                        // Set up POST body.
-                        var boundary = string.Format(BoundaryTemplate, Guid.NewGuid());
-                        var startFormData = BuildFormDataStart(boundary, filename);
-                        var endFormData = BuildFormDataEnd(boundary);
-                        var contentTypeHdrValue = string.Format(CONTENT_TYPE_HEADER_PARAMS, boundary);
-
-                        var json = BingImageSearch(startFormData, endFormData, imageBinary, contentTypeHdrValue);
-
-                        Console.WriteLine("\nJSON Response:\n");
-                        Console.WriteLine(JsonPrettyPrint(json));
-                    }
-                }
-                else
-                {
-                    Console.WriteLine("Invalid Bing Visual Search API subscription key!");
-                    Console.WriteLine("Please paste yours into the source code.");
+                    StreamWriter writer = new StreamWriter(requestStream);
+                    writer.Write(startFormData);
+                    writer.Flush();
+                    requestStream.Write(imageBinary, 0, imageBinary.Length);
+                    writer.Write(endFormData);
+                    writer.Flush();
+                    writer.Close();
                 }
 
-                Console.Write("\nPress Enter to exit ");
-                Console.ReadLine();
+                // Calls the Bing Visual Search endpoint and returns the JSON response.
+                HttpWebResponse response = (HttpWebResponse)request.GetResponseAsync().Result;
+                string json = new StreamReader(response.GetResponseStream()).ReadToEnd();
+
+                Console.WriteLine("\nJSON Response:\n");
+                dynamic parsedJson = JsonConvert.DeserializeObject(json);
+                Console.WriteLine(JsonConvert.SerializeObject(parsedJson, Formatting.Indented));
             }
             catch (Exception e)
             {
                 Console.WriteLine(e.Message);
             }
-        }
-
-
-
-        /// <summary>
-        /// Verify that imagePath exists.
-        /// </summary>
-        static Boolean IsImagePathSet(string path)
-        {
-            if (string.IsNullOrEmpty(path))
-                throw new ArgumentException("Image path is null or empty.");
-
-            if (!File.Exists(path))
-                throw new ArgumentException("Image path does not exist.");
-
-            var size = new FileInfo(path).Length;
-
-            if (size > 1000000)
-                throw new ArgumentException("Image is greater than the 1 MB maximum size.");
-
-            return true;
-        }
-
-
-
-        /// <summary>
-        /// Get the binary characters of an image.
-        /// </summary>
-        static byte[] GetImageBinary(string path)
-        {
-            return File.ReadAllBytes(path);
-        }
-
-
-        /// <summary>
-        /// Get the image's filename.
-        /// </summary>
-        static string GetImageFileName(string path)
-        {
-            return new FileInfo(path).Name;
-        }
-
-
-        /// <summary>
-        /// Build the beginning part of the form data.
-        /// </summary>
-        static string BuildFormDataStart(string boundary, string filename)
-        {
-            var startBoundary = string.Format(StartBoundaryTemplate, boundary);
-
-            var requestBody = startBoundary + CRLF;
-            requestBody += string.Format(POST_BODY_DISPOSITION_HEADER, filename);
-
-            return requestBody;
-        }
-
-
-        /// <summary>
-        /// Build the ending part of the form data.
-        /// </summary>
-        static string BuildFormDataEnd(string boundary)
-        {
-            return CRLF + CRLF + string.Format(EndBoundaryTemplate, boundary) + CRLF;
-        }
-
-
-
-        /// <summary>
-        /// Calls the Bing visual search endpoint and returns the JSON response.
-        /// </summary>
-        static string BingImageSearch(string startFormData, string endFormData, byte[] image, string contentTypeValue)
-        {
-            WebRequest request = HttpWebRequest.Create(uriBase);
-            request.ContentType = contentTypeValue;
-            request.Headers["Ocp-Apim-Subscription-Key"] = accessKey;
-            request.Method = "POST";
-
-            // Writes the boundary and Content-Disposition header, then writes
-            // the image binary, and finishes by writing the closing boundary.
-            using (Stream requestStream = request.GetRequestStream())
-            {
-                StreamWriter writer = new StreamWriter(requestStream);
-                writer.Write(startFormData);
-                writer.Flush();
-                requestStream.Write(image, 0, image.Length);
-                writer.Write(endFormData);
-                writer.Flush();
-                writer.Close();
-            }
-
-
-            HttpWebResponse response = (HttpWebResponse)request.GetResponseAsync().Result;
-            string json = new StreamReader(response.GetResponseStream()).ReadToEnd();
-
-            return json;
-        }
-
-
-        /// <summary>
-        /// Formats the given JSON string by adding line breaks and indents.
-        /// </summary>
-        /// <param name="json">The raw JSON string to format.</param>
-        /// <returns>The formatted JSON string.</returns>
-        static string JsonPrettyPrint(string json)
-        {
-            if (string.IsNullOrEmpty(json))
-                return string.Empty;
-
-            json = json.Replace(Environment.NewLine, "").Replace("\t", "");
-
-            StringBuilder sb = new StringBuilder();
-            bool quote = false;
-            bool ignore = false;
-            char last = ' ';
-            int offset = 0;
-            int indentLength = 2;
-
-            foreach (char ch in json)
-            {
-                switch (ch)
-                {
-                    case '"':
-                        if (!ignore) quote = !quote;
-                        break;
-                    case '\\':
-                        if (quote && last != '\\') ignore = true;
-                        break;
-                }
-
-                if (quote)
-                {
-                    sb.Append(ch);
-                    if (last == '\\' && ignore) ignore = false;
-                }
-                else
-                {
-                    switch (ch)
-                    {
-                        case '{':
-                        case '[':
-                            sb.Append(ch);
-                            sb.Append(Environment.NewLine);
-                            sb.Append(new string(' ', ++offset * indentLength));
-                            break;
-                        case '}':
-                        case ']':
-                            sb.Append(Environment.NewLine);
-                            sb.Append(new string(' ', --offset * indentLength));
-                            sb.Append(ch);
-                            break;
-                        case ',':
-                            sb.Append(ch);
-                            sb.Append(Environment.NewLine);
-                            sb.Append(new string(' ', offset * indentLength));
-                            break;
-                        case ':':
-                            sb.Append(ch);
-                            sb.Append(' ');
-                            break;
-                        default:
-                            if (quote || ch != ' ') sb.Append(ch);
-                            break;
-                    }
-                }
-                last = ch;
-            }
-
-            return sb.ToString().Trim();
         }
     }
 }
