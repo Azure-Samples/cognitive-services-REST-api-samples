eb8bed5063d010811c934205f63955acefb90b1a
Winona Azure <38537084+wiazur@users.noreply.github.com>
2020-01-23 19:42:27 -0800

    Updated and fixed bug

--- python/Language/BingSpellCheckv7.py
+++ python/Language/BingSpellCheckv7.py
@@ -1,8 +1,16 @@
 #Copyright (c) Microsoft Corporation. All rights reserved.
 #Licensed under the MIT License.
 
-import http.client, urllib.parse, json
+import http.client
+import json
 import os
+from pprint import pprint
+import urllib.parse
+
+'''
+This sample uses the Bing Spell Check API to check the spelling of query words and then suggests corrections.
+Bing Spell Check API: https://dev.cognitive.microsoft.com/docs/services/5f7d486e04d2430193e1ca8f760cd7ed/operations/57855119bca1df1c647bc358 
+'''
 
 text = 'Hollo, wrld!'
 
@@ -13,6 +21,7 @@ key = os.environ['BING_SPELL_CHECK_SUBSCRIPTION_KEY']
 
 # Add your Bing Spell Check endpoint to your environment variables.
 host = os.environ['BING_SPELL_CHECK_ENDPOINT']
+host = host.replace('https://', '')
 path = '/bing/v7.0/spellcheck'
 
 headers = {'Ocp-Apim-Subscription-Key': key,
@@ -29,4 +38,4 @@ conn = http.client.HTTPSConnection(host)
 params = urllib.parse.urlencode (params)
 conn.request ("POST", path, params, headers)
 response = conn.getresponse ()
-print (response.read ())
+pprint(json.loads(response.read()))
