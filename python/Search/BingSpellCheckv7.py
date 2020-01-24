1d704f515761595b13ca80688716b587f340cd56
Winona Azure <38537084+wiazur@users.noreply.github.com>
2020-01-24 13:12:31 -0800

    Updated structure

--- python/Search/BingSpellCheckv7.py
+++ python/Search/BingSpellCheckv7.py
@@ -1,26 +1,39 @@
 #Copyright (c) Microsoft Corporation. All rights reserved.
 #Licensed under the MIT License.
 
-import requests
+import http.client
 import json
+import os
+from pprint import pprint
+import urllib.parse
 
-api_key = "enter-your-key-here"
-example_text = "Hollo, wrld"
+'''
+This sample uses the Bing Spell Check API to check the spelling of query words and then suggests corrections.
+Bing Spell Check API: https://dev.cognitive.microsoft.com/docs/services/5f7d486e04d2430193e1ca8f760cd7ed/operations/57855119bca1df1c647bc358 
+'''
 
-endpoint = "https://api.cognitive.microsoft.com/bing/v7.0/SpellCheck"
+text = 'Hollo, wrld!'
 
-data = {'text': example_text}
+params = {'mkt': 'en-US', 'mode': 'proof', 'text': text}
 
-params = {
-    'mkt':'en-us',
-    'mode':'proof'
-    }
+# Add your Bing Spell Check subscription key anf endpoint to your environment variables.
+key = os.environ['BING_SPELL_CHECK_SUBSCRIPTION_KEY']
+host = os.environ['BING_SPELL_CHECK_ENDPOINT']
+host = host.replace('https://', '')
+path = '/bing/v7.0/spellcheck'
 
-headers = {
-    'Ocp-Apim-Subscription-Key': api_key,
-    'Content-Type': 'application/x-www-form-urlencoded'
-    }
+headers = {'Ocp-Apim-Subscription-Key': key,
+'Content-Type': 'application/x-www-form-urlencoded'}
 
-response = requests.post(endpoint, headers=headers, params=params, data=data)
-json_response = response.json()
-print(json.dumps(json_response, indent=4))
+# The headers in the following example 
+# are optional but should be considered as required:
+#
+# X-MSEdge-ClientIP: 999.999.999.999  
+# X-Search-Location: lat: +90.0000000000000;long: 00.0000000000000;re:100.000000000000
+# X-MSEdge-ClientID: <Client ID from Previous Response Goes Here>
+
+conn = http.client.HTTPSConnection(host)
+params = urllib.parse.urlencode (params)
+conn.request ("POST", path, params, headers)
+response = conn.getresponse ()
+pprint(json.loads(response.read()))
