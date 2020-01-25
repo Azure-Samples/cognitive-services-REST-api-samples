f2285cad7c4a05d1eb358af8a44ef66857cfdc49
Winona Azure <38537084+wiazur@users.noreply.github.com>
2020-01-24 19:43:12 -0800

    Updated library

--- python/Search/BingVideoSearchv7.py
+++ python/Search/BingVideoSearchv7.py
@@ -1,41 +1,40 @@
-#Copyright (c) Microsoft Corporation. All rights reserved.
-#Licensed under the MIT License.
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
 
 # -*- coding: utf-8 -*-
 
-import http.client
 import json
 import os
-import urllib.parse
+from pprint import pprint
+import requests
 
 '''
-This sample makes a call to the Bing Video Search API with a topic query and returns relevant video links with data.
+This sample makes a call to the Bing Video Search API with a topic query and returns relevant video with data.
 Documentation: https: // docs.microsoft.com/en-us/azure/cognitive-services/bing-web-search/
 '''
 
 # Add your Bing Search V7 subscription key and endpoint to your environment variables.
 subscriptionKey = os.environ['BING_SEARCH_V7_SUBSCRIPTION_KEY']
 host = os.environ['BING_SEARCH_V7_ENDPOINT']
-host = host.replace('https://', '')
 path = "/bing/v7.0/videos/search"
 
 # Search query
-term = "kittens"
+query = "kittens"
 
 # Construct a request
-headers = {'Ocp-Apim-Subscription-Key': subscriptionKey}
-conn = http.client.HTTPSConnection(host)
-query = urllib.parse.quote(term)
+headers = {
+    'Content-Type': 'application/json',
+    'Ocp-Apim-Subscription-Key': subscriptionKey
+    }
+params = { "q": query }
 
 # Call the API
-print('Searching videos for: ', term)
-conn.request("GET", path + "?q=" + query, headers=headers)
-response = conn.getresponse()
-headers = [k + ": " + v for (k, v) in response.getheaders()
-           if k.startswith("BingAPIs-") or k.startswith("X-MSEdge-")]
+response = requests.get(host + path, headers=headers, params=params)
+response.raise_for_status()
 
 # Print results
-print("\nRelevant HTTP Headers:\n")
-print("\n".join(headers))
+print("\nHeaders:\n")
+print(response.headers)
+
 print("\nJSON Response:\n")
-print(json.dumps(json.loads(response.read()), indent=4))
+pprint(response.json())
