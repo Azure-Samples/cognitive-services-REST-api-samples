59f04174e9fec8d54d8054559aff8031f76eaa12
Winona Azure <38537084+wiazur@users.noreply.github.com>
2020-01-27 13:31:15 -0800

    Updated library

--- python/Search/BingNewsSearchv7.py
+++ python/Search/BingNewsSearchv7.py
@@ -3,10 +3,10 @@
 
 # -*- coding: utf-8 -*-
 
-import http.client
 import json
-import urllib.parse
 import os
+from pprint import pprint
+import requests
 
 '''
 This sample makes a call to the Bing News Search API with a text query and returns relevant news webpages.
@@ -15,24 +15,24 @@ Documentation: https: // docs.microsoft.com/en-us/azure/cognitive-services/bing-
 
 # Add your Bing Search V7 subscription key and endpoint to your environment variables.
 subscriptionKey = os.environ['BING_SEARCH_V7_SUBSCRIPTION_KEY']
-host = os.environ['BING_SEARCH_V7_ENDPOINT']
-host = host.replace('https://', '')
-path = "/bing/v7.0/news/search"
+endpoint = os.environ['BING_SEARCH_V7_ENDPOINT'] + "/bing/v7.0/news/search"
 
-term = "Microsoft"
+query = "Microsoft"
 
-# Construct the request.
+# Construct a request
+mkt = 'en-US'
+params = {'q': query, 'mkt': mkt}
 headers = {'Ocp-Apim-Subscription-Key': subscriptionKey}
-conn = http.client.HTTPSConnection(host)
-query = urllib.parse.quote(term)
-print('Searching news for: ', term)
-conn.request("GET", path + "?q=" + query, headers=headers)
-response = conn.getresponse()
-headers = [k + ": " + v for (k, v) in response.getheaders()
-           if k.startswith("BingAPIs-") or k.startswith("X-MSEdge-")]
 
-# Print search results. 
-print("\nRelevant HTTP Headers:\n")
-print("\n".join(headers))
-print("\nJSON Response:\n")
-print(json.dumps(json.loads(response.read()), indent=4))
+# Call the API
+try:
+    response = requests.get(endpoint, headers=headers, params=params)
+    response.raise_for_status()
+
+    print("\nHeaders:\n")
+    print(response.headers)
+
+    print("\nJSON Response:\n")
+    pprint(response.json())
+except Exception as ex:
+    raise ex
