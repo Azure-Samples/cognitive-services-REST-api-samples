59f82ccb38baaffe16f7be1997df6157f21b40e0
Winona Azure <38537084+wiazur@users.noreply.github.com>
2020-01-27 17:47:30 -0800

    Minor

--- python/Vision/ComputerVision2.1.py
+++ python/Vision/ComputerVision2.1.py
@@ -12,7 +12,7 @@ and then returns user input parameter data like category, description, and color
 API: https://westus.dev.cognitive.microsoft.com/docs/services/5adf991815e1060e6355ad44/operations/587f2c6a154055056008f200
 '''
 
-# Add your Bing Search V7 subscription key and endpoint to your environment variables.
+# Add your Computer Vision subscription key and endpoint to your environment variables.
 subscription_key = os.environ['COMPUTER_VISION_SUBSCRIPTION_KEY']
 endpoint = os.environ['COMPUTER_VISION_ENDPOINT'] + "/vision/v2.1/analyze"
 
@@ -22,16 +22,16 @@ headers = {
     'Ocp-Apim-Subscription-Key': subscription_key,
 }
 
-# Request parameters. All of them are optional
+# Request parameters. All of them are optional.
 params = {
     'visualFeatures': 'Categories,Description,Color',
     'language': 'en',
 }
 
-# Replace the three dots below with the URL of a JPEG image of a celebrity.
+# Any image with objects will work.
 body = {'url': 'https://raw.githubusercontent.com/Azure-Samples/cognitive-services-sample-data-files/master/ComputerVision/Images/objects.jpg'}
 
-# Call the API
+# Call the API.
 try:
     response = requests.post(endpoint, headers=headers, params=params, json=body)
     response.raise_for_status()
