00709dd95a221c528529f57733c745f64e6d19bd
Winona Azure <38537084+wiazur@users.noreply.github.com>
2020-02-04 20:37:39 -0800

    Updated sample

--- java/Search/BingSpellCheckv7.java
+++ java/Search/BingSpellCheckv7.java
@@ -1,50 +1,67 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 import java.io.*;
 import java.net.*;
 import javax.net.ssl.HttpsURLConnection;
+import org.json.*;
 
-public class HelloWorld {
+/**
+ * This sample uses the Bing Spell Check v7 API to check spelling of a sentence.
+ *
+ * Include library (for pretty print), add jar to a lib folder:
+ * https://github.com/stleary/JSON-java
+ *
+ * Add your subscription key and endpoint to your environment variables.
+ *
+ * Build/run from command line: 
+ *   javac BingSpellCheck.java -cp .;lib\* 
+ *   java -cp .;lib\* BingSpellCheck
+ */
 
-    // Add your Bing Spell Check subscription key to your environment variables.
-    static String host = System.getenv("BING_SPELL_CHECK_ENDPOINT");
-    static String path = "/bing/v7.0/spellcheck";
+public class BingSpellCheck {
 
-    // Add your Bing Spell Check endpoint to your environment variables.
-    static String key = System.getenv("BING_SPELL_CHECK_SUBSCRIPTION_KEY");
+    // Or using the generic endpoint https://api.cognitive.microsoft.com is OK too.
+    static String endpoint = System.getenv("BING_SPELL_CHECK_ENDPOINT") + "/bing/v7.0/spellcheck";
+
+    // NOTE: Replace this example key with a valid subscription key.
+    static String subscriptionKey = System.getenv("BING_SPELL_CHECK_SUBSCRIPTION_KEY");
 
     static String mkt = "en-US";
     static String mode = "proof";
     static String text = "Hollo, wrld!";
 
-    public static void check () throws Exception {
-		String params = "?mkt=" + mkt + "&mode=" + mode;
-        URL url = new URL(host + path + params);
-        HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
-        connection.setRequestMethod("POST");
-        connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-		connection.setRequestProperty("Content-Length", "" + text.length() + 5);
-        connection.setRequestProperty("Ocp-Apim-Subscription-Key", key);
-        connection.setDoOutput(true);
-
-        DataOutputStream wr = new DataOutputStream(connection.getOutputStream());
-        wr.writeBytes("text=" + text);
-        wr.flush();
-        wr.close();
-
-        BufferedReader in = new BufferedReader(
-        new InputStreamReader(connection.getInputStream()));
-        String line;
-        while ((line = in.readLine()) != null) {
-            System.out.println(line);
-        }
-        in.close();
-    }
-
     public static void main(String[] args) {
         try {
-            check ();
-        }
-        catch (Exception e) {
-            System.out.println (e);
+            String params = "?mkt=" + mkt + "&mode=" + mode;
+            URL url = new URL(endpoint + params);
+            HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+            connection.setRequestProperty("Content-Length", "" + text.length() + 5);
+            connection.setRequestProperty("Ocp-Apim-Subscription-Key", subscriptionKey);
+            connection.setDoOutput(true);
+
+            // Optional
+            DataOutputStream wr = new DataOutputStream(connection.getOutputStream());
+            wr.writeBytes("text=" + text);
+            wr.flush();
+            wr.close();
+
+            // Get results
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+            String line;
+            StringBuilder sb = new StringBuilder();
+            while ((line = in.readLine()) != null) {
+                sb.append(line);
+            }
+            // Pretty print
+            System.out.println((new JSONObject(sb.toString())).toString(4));
+
+            in.close();
+        } catch (Exception e) {
+            System.out.println(e);
         }
     }
 }
