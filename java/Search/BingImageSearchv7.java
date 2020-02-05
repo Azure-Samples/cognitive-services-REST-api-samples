ae5ba8322674dfb13fbe730c26bfbe1d22bf3b55
Winona Azure <38537084+wiazur@users.noreply.github.com>
2020-02-04 19:43:03 -0800

    Updated sample, replaced deprecated code

--- java/Search/BingImageSearchv7.java
+++ java/Search/BingImageSearchv7.java
@@ -1,85 +1,36 @@
-/*Copyright (c) Microsoft Corporation. All rights reserved.
-Licensed under the MIT License.*/
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
 
 import java.net.*;
 import java.util.*;
 import java.io.*;
 import javax.net.ssl.HttpsURLConnection;
-
-/*
- * Gson: https://github.com/google/gson
- * Maven info:
- *     groupId: com.google.code.gson
- *     artifactId: gson
- *     version: 2.8.1
- *
- * Once you have compiled or downloaded gson-2.8.1.jar, assuming you have placed it in the
- * same folder as this file (BingImageSearch.java), you can compile and run this program at
- * the command line as follows.
- *
- * javac BingImageSearch.java -classpath .;gson-2.8.1.jar -encoding UTF-8
- * java -cp .;gson-2.8.1.jar BingImageSearch
- */
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
+/*
+ * Gson: https://github.com/google/gson
+ * Maven info:
+ *   groupId: com.google.code.gson
+ *   artifactId: gson
+ *   version: x.x.x
+ *
+ * Compile and run from the command line:
+ *   javac BingImageSearch.java -classpath .;gson-2.8.6.jar -encoding UTF-8
+ *   java -cp .;gson-2.8.6.jar BingImageSearch
+ */
+
 public class BingImageSearch {
-
-// ***********************************************
-// *** Update or verify the following values. ***
-// **********************************************
-
     // Add your Bing Search V7 subscription key to your environment variables.
     static String subscriptionKey = System.getenv("BING_SEARCH_V7_SUBSCRIPTION_KEY");
     // Add your Bing Search V7 endpoint to your environment variables.
-    static String host = System.getenv("BING_SEARCH_V7_ENDPOINT");
-    static String path = "/bing/v7.0/images/search";
+    static String endpoint = System.getenv("BING_SEARCH_V7_ENDPOINT") + "/bing/v7.0/images/search";
 
     static String searchTerm = "puppies";
 
-    public static SearchResults SearchImages (String searchQuery) throws Exception {
-        // construct URL of search request (endpoint + query string)
-        URL url = new URL(host + path + "?q=" +  URLEncoder.encode(searchQuery, "UTF-8"));
-        HttpsURLConnection connection = (HttpsURLConnection)url.openConnection();
-        connection.setRequestProperty("Ocp-Apim-Subscription-Key", subscriptionKey);
-
-        // receive JSON body
-        InputStream stream = connection.getInputStream();
-        String response = new Scanner(stream).useDelimiter("\\A").next();
-
-        // construct result object for return
-        SearchResults results = new SearchResults(new HashMap<String, String>(), response);
-
-        // extract Bing-related HTTP headers
-        Map<String, List<String>> headers = connection.getHeaderFields();
-        for (String header : headers.keySet()) {
-            if (header == null) continue;      // may have null key
-            if (header.startsWith("BingAPIs-") || header.startsWith("X-MSEdge-")) {
-                results.relevantHeaders.put(header, headers.get(header).get(0));
-            }
-        }
-
-        stream.close();
-        return results;
-    }
-
-    // pretty-printer for JSON; uses GSON parser to parse and re-serialize
-    public static String prettify(String json_text) {
-        JsonParser parser = new JsonParser();
-        JsonObject json = parser.parse(json_text).getAsJsonObject();
-        Gson gson = new GsonBuilder().setPrettyPrinting().create();
-        return gson.toJson(json);
-    }
-
-    public static void main (String[] args) {
-        if (subscriptionKey.length() != 32) {
-            System.out.println("Invalid Bing Search API subscription key!");
-            System.out.println("Please paste yours into the source code.");
-            System.exit(1);
-        }
-
+    public static void main(String[] args) {
         try {
             System.out.println("Searching the Web for: " + searchTerm);
 
@@ -91,11 +42,46 @@ public class BingImageSearch {
 
             System.out.println("\nJSON Response:\n");
             System.out.println(prettify(result.jsonResponse));
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             e.printStackTrace(System.out);
             System.exit(1);
         }
+    }
+
+    public static SearchResults SearchImages (String searchQuery) throws Exception {
+        // Construct URL of search request (endpoint + query string)
+        URL url = new URL(endpoint + "?q=" +  URLEncoder.encode(searchQuery, "UTF-8"));
+        HttpsURLConnection connection = (HttpsURLConnection)url.openConnection();
+        connection.setRequestProperty("Ocp-Apim-Subscription-Key", subscriptionKey);
+
+        // Receive JSON body
+        InputStream stream = connection.getInputStream();
+        Scanner scanner = new Scanner(stream);
+        String response = scanner.useDelimiter("\\A").next();
+
+        // Construct result object for return
+        SearchResults results = new SearchResults(new HashMap<String, String>(), response);
+
+        // Extract Bing-related HTTP headers
+        Map<String, List<String>> headers = connection.getHeaderFields();
+        for (String header : headers.keySet()) {
+            if (header == null) continue;      // may have null key
+            if (header.startsWith("BingAPIs-") || header.startsWith("X-MSEdge-")) {
+                results.relevantHeaders.put(header, headers.get(header).get(0));
+            }
+        }
+
+        scanner.close();
+        stream.close();
+
+        return results;
+    }
+
+    // pretty-printer for JSON; uses GSON parser to parse and re-serialize
+    public static String prettify(String json_text) {
+        JsonObject json = JsonParser.parseString(json_text).getAsJsonObject();
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        return gson.toJson(json);
     }
 }
 
