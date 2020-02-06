11e62e26ca154fdd93d920648e9607f540811f89
Winona Azure <38537084+wiazur@users.noreply.github.com>
2020-02-06 15:39:46 -0800

    Updated libraries

--- nodejs/Language/BingSpellCheckv7.js
+++ nodejs/Language/BingSpellCheckv7.js
@@ -1,53 +1,58 @@
-//Copyright (c) Microsoft Corporation. All rights reserved.
-//Licensed under the MIT License.
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
 
 'use strict';
 
-let https = require ('https');
+const request = require ('request')
 
-let host = 'api.cognitive.microsoft.com';
-let path = '/bing/v7.0/spellcheck';
+/** 
+ * This sample uses the Bing Spell Check API to check the spelling of a sentence. 
+ * It returns the identified misspellings with suggestions for correctly spelled words
+ * Plus, a score of what are deemed to be the best matches for suggestions. 
+ */
 
-/* NOTE: Replace this example key with a valid subscription key (see the Prequisites section above). Also note v5 and v7 require separate subscription keys. */
-let key = 'ENTER KEY HERE';
+// Add your Bing Spell Check key and endpoint (host) to your environment variables.
+// Note v5 and v7 require separate subscription keys.
+let key = process.env['BING_SPELL_CHECK_SUBSCRIPTION_KEY']
+let endpoint = process.env['BING_SPELL_CHECK_ENDPOINT'] + '/bing/v7.0/spellcheck/'
+
+let text = "Hollo, wrld!"
+let mode = "proof"
+let mkt = "en-US"
 
 // These values are used for optional headers (see below).
 // let CLIENT_ID = "<Client ID from Previous Response Goes Here>";
 // let CLIENT_IP = "999.999.999.999";
 // let CLIENT_LOCATION = "+90.0000000000000;long: 00.0000000000000;re:100.000000000000";
 
-let mkt = "en-US";
-let mode = "proof";
-let text = "Hollo, wrld!";
-let query_string = "?mkt=" + mkt + "&mode=" + mode;
+let headers = {
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Content-Length': text.length + 5,
+    'Ocp-Apim-Subscription-Key': key
+    // Optional Headers
+    // 'X-Search-Location' : CLIENT_LOCATION,
+    // 'X-MSEdge-ClientID' : CLIENT_ID,
+    // 'X-MSEdge-ClientIP' : CLIENT_ID,
+}
 
 let request_params = {
-    method : 'POST',
-    hostname : host,
-    path : path + query_string,
-    headers : {
-        'Content-Type' : 'application/x-www-form-urlencoded',
-        'Content-Length' : text.length + 5,
-        'Ocp-Apim-Subscription-Key' : key,
-//        'X-Search-Location' : CLIENT_LOCATION,
-//        'X-MSEdge-ClientID' : CLIENT_ID,
-//        'X-MSEdge-ClientIP' : CLIENT_ID,
-    }
-};
+    method: 'POST',
+    url: endpoint,
+    headers: headers,
+    qs: { 
+        mode: mode,
+        mkt: mkt,
+        text: text 
+    },
+    json: true
+}
 
-let response_handler = function (response) {
-    let body = '';
-    response.on ('data', function (d) {
-        body += d;
-    });
-    response.on ('end', function () {
-        console.log (body);
-    });
-    response.on ('error', function (e) {
-        console.log ('Error: ' + e.message);
-    });
-};
+request(request_params, function (error, response, body) {
+    console.error('error:', error)
+    console.log('statusCode:', response && response.statusCode)
 
-let req = https.request (request_params, response_handler);
-req.write ("text=" + text);
-req.end ();
+    // Print suggestions for each misspelled word (token)
+     body.flaggedTokens.forEach(token => {
+        console.log(token)
+    }) 
+})
