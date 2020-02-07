43f1203da40b33e6951f8c85cf702b8fdce90094
Winona Azure <38537084+wiazur@users.noreply.github.com>
2020-02-06 18:05:58 -0800

    Updated library

--- nodejs/Search/BingCustomSearchv7.js
+++ nodejs/Search/BingCustomSearchv7.js
@@ -1,36 +1,52 @@
-//Copyright (c) Microsoft Corporation. All rights reserved.
-//Licensed under the MIT License.
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 // <vars>
 var request = require("request");
-// Add your Bing Custom Search subscription key to your environment variables.
-// Your endpoint will have the form: 
-//   https://<your-custom-subdomain>.cognitiveservices.azure.com/bingcustomsearch/v7.0
+
+/**
+ * This sample uses the Bing Custom Search API to send a customized query
+ * that returns a lot of data about the query with results.
+ */
+
+// Add your Bing Custom Search subscription key and endpoint to your environment variables.
 var subscriptionKey = process.env['BING_CUSTOM_SEARCH_SUBSCRIPTION_KEY'];
-var endpoint = process.env['BING_CUSTOM_SEARCH_ENDPOINT'];
-var customConfigId = 'YOUR-CUSTOM-CONFIG-ID'; //you can also use "1"
-var searchTerm = 'microsoft';
+var endpoint = process.env['BING_CUSTOM_SEARCH_ENDPOINT'] + "/bingcustomsearch/v7.0/search?";
+
+var customConfigId = process.env['BING_CUSTOM_CONFIG']; //you can also use "1"
+
+// Word(s) you want to search for.
+var query = 'Microsoft';
+// Market you want to search in.
+let mkt = 'en-US'
 // </vars>
+
 // <requestOptions>
-var options = {
-    url: endpoint + "/search?" + 
-      'q=' + searchTerm + 
-      '&customconfig=' + customConfigId,
+// Construct parameters
+let request_params = {
+    uri: endpoint,
     headers: {
-        'Ocp-Apim-Subscription-Key' : subscriptionKey
-    }
+        'Ocp-Apim-Subscription-Key': subscriptionKey
+    },
+    qs: {
+        customConfig: customConfigId,
+        q: query,
+        mkt: mkt
+    },
+    json: true
 }
 // </requestOptions>
+
 // <requestMethod>
-request(options, function(error, response, body){
-    var searchResponse = JSON.parse(body);
-    for(var i = 0; i < searchResponse.webPages.value.length; ++i){
-        var webPage = searchResponse.webPages.value[i];
-        console.log('name: ' + webPage.name);
-        console.log('url: ' + webPage.url);
-        console.log('displayUrl: ' + webPage.displayUrl);
-        console.log('snippet: ' + webPage.snippet);
-        console.log('dateLastCrawled: ' + webPage.dateLastCrawled);
-        console.log();
-    }
+// Make request
+request(request_params, function (error, response, body) {
+    console.error('error:', error)
+    console.log('statusCode:', response && response.statusCode)
+
+    console.log(body.queryContext)
+    console.log()
+    body.webPages.value.forEach(v => {
+        console.log(v)
+    })
 })
 // </requestMethod>
